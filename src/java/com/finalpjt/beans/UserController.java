12b532f241e81fe62025fd2899d0a172a9257133
rakotomalala <rakotomalala.francky@gmail.com>
2018-06-01 12:01:36 +0200

    service & action page

--- src/java/com/finalpjt/beans/UserController.java
+++ src/java/com/finalpjt/beans/UserController.java
@@ -1,21 +1,73 @@
 
 package com.finalpjt.beans;
 
-import java.util.Date;
+import com.finalpjt.entity.User;
+import com.finalpjt.service.UserService;
+import javax.ejb.EJB;
 
 
 public class UserController {
 
-    private Long id;
-    private String userName;
+    private Long idUser;
+    private String firstName;
+    private String lastName;
+    private String login;
     private String password;
-    private String firstname;
-    private String lastname;
-    private String emailname;
-    private Date birthday;
+    private String address;
     
-    public UserController() {
+    @EJB
+    private UserService userService;
+    
+    public UserController() {}
+    
+    public void addUser() {
         
     }
+    
+    public User getUser() {
+        return null;
+    }
+    
+    public Long getIdUser() {
+        return idUser;
+    }
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+    
+    
+    
     
 }
